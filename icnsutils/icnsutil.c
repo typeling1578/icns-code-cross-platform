typeling1578/icns-code-cross-platform

commit 382937bb38ff1ab3513a7ef232b2a562433ec1be

Add support for 64px ARGB (icp6) icons
Thanks to Luflosi for uploading the sample:

https://github.com/kornelski/libicns/issues/2#issuecomment-704879267

icnsutils/icnsutil.c
@@ -60,6 +60,7 @@ static const icns_type_t iconset_types[] = {
 	ICNS_16x16_2X_32BIT_ARGB_DATA,
 	ICNS_32x32_32BIT_DATA,
 	ICNS_32x32_2X_32BIT_ARGB_DATA,
+	ICNS_64x64_32BIT_ARGB_DATA,
 	ICNS_128X128_32BIT_DATA,
 	ICNS_128x128_2X_32BIT_ARGB_DATA,
 	ICNS_128x128_32BIT_ARGB_DATA,

src/icns.h
@@ -101,6 +101,7 @@ typedef struct icns_icon_info_t
 #define ICNS_512x512_32BIT_ARGB_DATA  0x69633039  // "ic09"
 #define ICNS_256x256_32BIT_ARGB_DATA  0x69633038  // "ic08"
 #define ICNS_128x128_32BIT_ARGB_DATA  0x69633037  // "ic07"
+#define ICNS_64x64_32BIT_ARGB_DATA    0x69637036  // "icp6"
 
 #define ICNS_128X128_32BIT_DATA       0x69743332  // "it32"
 #define ICNS_128X128_8BIT_MASK        0x74386D6B  // "t8mk"

src/icns_element.c
@@ -568,6 +568,7 @@ int icns_update_element_with_image_or_mask(icns_image_t *imageIn,icns_bool_t isM
 	case ICNS_128x128_2X_32BIT_ARGB_DATA:
 	case ICNS_32x32_2X_32BIT_ARGB_DATA:
 	case ICNS_16x16_2X_32BIT_ARGB_DATA:
+	case ICNS_64x64_32BIT_ARGB_DATA:
 	//case ICNS_1024x1024_32BIT_ARGB_DATA:
 	case ICNS_128x128_32BIT_ARGB_DATA:
 	case ICNS_256x256_32BIT_ARGB_DATA:

src/icns_image.c
@@ -106,6 +106,7 @@ int icns_get_image32_with_mask_from_family(icns_family_t *iconFamily,icns_type_t
 		(iconType == ICNS_1024x1024_32BIT_ARGB_DATA) ||
 		(iconType == ICNS_16x16_2X_32BIT_ARGB_DATA) ||
 		(iconType == ICNS_32x32_2X_32BIT_ARGB_DATA) ||
+		(iconType == ICNS_64x64_32BIT_ARGB_DATA) ||
 		(iconType == ICNS_128x128_2X_32BIT_ARGB_DATA) ||
 		(iconType == ICNS_256x256_2X_32BIT_ARGB_DATA) ||
 		(iconType == ICNS_512x512_2X_32BIT_ARGB_DATA)
@@ -432,6 +433,7 @@ int icns_get_image_from_element(icns_element_t *iconElement,icns_image_t *imageO
 		case ICNS_512x512_32BIT_ARGB_DATA:
 		case ICNS_256x256_32BIT_ARGB_DATA:
 		case ICNS_128x128_32BIT_ARGB_DATA:
+		case ICNS_64x64_32BIT_ARGB_DATA:
 			{
 				uint8_t magicPNG[] = {0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A};
 				uint8_t magicByt[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};

src/icns_utils.c
@@ -53,6 +53,7 @@ icns_uint32_t icns_get_element_order(icns_type_t iconType)
 	case ICNS_128x128_32BIT_ARGB_DATA:
 	case ICNS_128x128_2X_32BIT_ARGB_DATA:
 		return 25;
+	case ICNS_64x64_32BIT_ARGB_DATA:
 	case ICNS_32x32_2X_32BIT_ARGB_DATA:
 		return 24;
 	case ICNS_16x16_2X_32BIT_ARGB_DATA:
@@ -139,6 +140,8 @@ icns_type_t icns_get_mask_type_for_icon_type(icns_type_t iconType)
 	case ICNS_256x256_32BIT_ARGB_DATA:
 		return ICNS_NULL_MASK;
 	case ICNS_128x128_32BIT_ARGB_DATA:
+		return ICNS_NULL_MASK;
+	case ICNS_64x64_32BIT_ARGB_DATA:
 		return ICNS_NULL_MASK;
 
 	// 32-bit image types - 8-bit mask type
@@ -302,6 +305,15 @@ icns_icon_info_t icns_get_image_info_for_type(icns_type_t iconType)
 		iconInfo.isMask = 0;
 		iconInfo.iconWidth = 128;
 		iconInfo.iconHeight = 128;
+		iconInfo.iconChannels = 4;
+		iconInfo.iconPixelDepth = 8;
+		iconInfo.iconBitDepth = 32;
+		break;
+	case ICNS_64x64_32BIT_ARGB_DATA:
+		iconInfo.isImage = 1;
+		iconInfo.isMask = 0;
+		iconInfo.iconWidth = 64;
+		iconInfo.iconHeight = 64;
 		iconInfo.iconChannels = 4;
 		iconInfo.iconPixelDepth = 8;
 		iconInfo.iconBitDepth = 32;
@@ -681,7 +693,7 @@ icns_type_t	icns_get_type_from_image_info_advanced(icns_icon_info_t iconInfo, ic
 		if(iconInfo.iconBitDepth == 32)
 			return ICNS_32x32_2X_32BIT_ARGB_DATA;
 		else
-			return ICNS_NULL_TYPE;
+			return ICNS_64x64_32BIT_ARGB_DATA;
 		break;
 	case 128:
 		if(iconInfo.isImage == 1 || iconInfo.iconBitDepth == 32)

commit 182704ef8b0bb147892e9def8350ba50b9a09e75

icnsutil: add missing icon names
Fixes: commit 382937bb38ff1ab3513a7ef232b2a562433ec1be
Fixes: commit efdbe5260432d39c2a43d365c89e8ef9e04dcc9d

icnsutils/icnsutil.c
@@ -46,8 +46,10 @@ static const char *iconset_names[] = {
 	"/icon_16x16@2x.png",
 	"/icon_32x32.png",
 	"/icon_32x32@2x.png",
+	"/icon_64x64.png",
 	"/icon_128x128.png",
 	"/icon_128x128@2x.png",
+	"/icon_128x128.png",
 	"/icon_256x256.png",
 	"/icon_256x256@2x.png",
 	"/icon_512x512.png",