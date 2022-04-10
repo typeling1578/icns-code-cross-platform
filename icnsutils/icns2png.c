typeling1578/icns-code-cross-platform

commit 78e85226cc9c0c38394c08eba9a0dc3fb8dc3c50

Implement 3510157: add read and write support for 1024x1024 icons.

icnsutils/icns2png.c
@@ -67,8 +67,8 @@ int	extractIconDepth = ALL_DEPTHS;
 /* Optional output directory */
 char    *outputPath = NULL;
 
-const char *sizeStrs[] =  { "512", "512x512", "256", "256x256", "128", "128x128", "48", "48x48", "32", "32x32", "16", "16x16", "16x12"    };
-const int   sizeVals[] =  {  512,   512,       256,   256,       128,   128,       48,   48,      32,   32,      16,   16,      MINI_SIZE };
+const char *sizeStrs[] =  { "1024", "1024x1024" "512", "512x512", "256", "256x256", "128", "128x128", "48", "48x48", "32", "32x32", "16", "16x16", "16x12"    };
+const int   sizeVals[] =  {  1024,   1024,       512,   512,       256,   256,       128,   128,       48,   48,      32,   32,      16,   16,      MINI_SIZE };
 
 const char *depthStrs[] = { "32", "8", "4", "1" };
 const int   depthVals[] = {  32 ,  8 ,  4 ,  1  };

icnsutils/png2icns.c
@@ -243,7 +243,7 @@ static int add_png_to_family(icns_family_t **iconFamily, char *pngname)
 	
 	icns_set_print_errors(1);
 	
-	if( (iconType != ICNS_512x512_32BIT_ARGB_DATA) && (iconType != ICNS_256x256_32BIT_ARGB_DATA) )
+	if( (iconType != ICNS_1024x1024_32BIT_ARGB_DATA) && (iconType != ICNS_512x512_32BIT_ARGB_DATA) && (iconType != ICNS_256x256_32BIT_ARGB_DATA) )
 	{
 		printf("Using icns type '%s', mask '%s' for '%s'\n", iconStr, maskStr, pngname);
 	}
@@ -263,7 +263,7 @@ static int add_png_to_family(icns_family_t **iconFamily, char *pngname)
 		free(iconElement);
 	}
 
-	if( (iconType != ICNS_512x512_32BIT_ARGB_DATA) && (iconType != ICNS_256x256_32BIT_ARGB_DATA) )
+	if( (iconType != ICNS_1024x1024_32BIT_ARGB_DATA) && (iconType != ICNS_512x512_32BIT_ARGB_DATA) && (iconType != ICNS_256x256_32BIT_ARGB_DATA) )
 	{
 		icns_init_image_for_type(maskType, &icnsMask);
 

src/icns_element.c
@@ -564,6 +564,7 @@ int icns_update_element_with_image_or_mask(icns_image_t *imageIn,icns_bool_t isM
 	
 	switch(iconType)
 	{
+	case ICNS_1024x1024_32BIT_ARGB_DATA:
 	case ICNS_256x256_32BIT_ARGB_DATA:
 	case ICNS_512x512_32BIT_ARGB_DATA:
 		error = icns_image_to_jp2(imageIn,&newDataSize,&newDataPtr);

src/icns_image.c
@@ -99,7 +99,7 @@ int icns_get_image32_with_mask_from_family(icns_family_t *iconFamily,icns_type_t
 	}
 	
 	// We used the jp2 processor for these two, so we're done!
-	if( (iconType == ICNS_256x256_32BIT_ARGB_DATA) || (iconType == ICNS_512x512_32BIT_ARGB_DATA) ) {
+	if( (iconType == ICNS_256x256_32BIT_ARGB_DATA) || (iconType == ICNS_512x512_32BIT_ARGB_DATA) || (iconType == ICNS_1024x1024_32BIT_ARGB_DATA) ) {
 		memcpy(imageOut,&iconImage,sizeof(icns_image_t));
 		if(iconElement != NULL) {
 		free(iconElement);
@@ -415,6 +415,7 @@ int icns_get_image_from_element(icns_element_t *iconElement,icns_image_t *imageO
 		// 32-Bit Icon Image Data Types ( > 256px )
 		case ICNS_256x256_32BIT_ARGB_DATA:
 		case ICNS_512x512_32BIT_ARGB_DATA:
+		case ICNS_1024x1024_32BIT_ARGB_DATA:
 			{
 				uint8_t magicPNG[] = {0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A};
 				uint8_t magicByt[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}; 

src/icns_internals.h
@@ -38,7 +38,7 @@ Boston, MA 02110-1301, USA.
 // Enable debugging messages?
 // #define	ICNS_DEBUG	1
 
-// Enable supprt for 256x256 and 512x512 icons
+// Enable supprt for 256x256, 512x512 and 1024x1024 icons
 // Use either Jasper or OpenJPEG, but not both
 // #define	ICNS_JASPER
 // #define	ICNS_OPENJPEG

src/icns_utils.c
@@ -46,6 +46,8 @@ icns_uint32_t icns_get_element_order(icns_type_t iconType)
 	{
 	case ICNS_ICON_VERSION:
 		return 100;
+	case ICNS_1024x1024_32BIT_ARGB_DATA:
+		return 23;
 	case ICNS_512x512_32BIT_ARGB_DATA:
 		return 22;
 	case ICNS_256x256_32BIT_ARGB_DATA:
@@ -112,6 +114,8 @@ icns_type_t icns_get_mask_type_for_icon_type(icns_type_t iconType)
 		return ICNS_NULL_MASK;
 		
 	// 32-bit image types > 256x256 - no mask (mask is already in image)
+	case ICNS_1024x1024_32BIT_ARGB_DATA:
+		return ICNS_NULL_MASK;
 	case ICNS_512x512_32BIT_ARGB_DATA:
 		return ICNS_NULL_MASK;			
 	case ICNS_256x256_32BIT_ARGB_DATA:
@@ -209,6 +213,15 @@ icns_icon_info_t icns_get_image_info_for_type(icns_type_t iconType)
 		iconInfo.iconBitDepth = 0;
 		break;
 	// 32-bit image types
+	case ICNS_1024x1024_32BIT_ARGB_DATA:
+		iconInfo.isImage = 1;
+		iconInfo.isMask = 0;
+		iconInfo.iconWidth = 1024;
+		iconInfo.iconHeight = 1024;
+		iconInfo.iconChannels = 4;
+		iconInfo.iconPixelDepth = 8;
+		iconInfo.iconBitDepth = 32;
+		break;
 	case ICNS_512x512_32BIT_ARGB_DATA:
 		iconInfo.isImage = 1;
 		iconInfo.isMask = 0;
@@ -607,6 +620,9 @@ icns_type_t	icns_get_type_from_image_info(icns_icon_info_t iconInfo)
 		break;
 	case 512:
 		return ICNS_512x512_32BIT_ARGB_DATA;
+		break;
+	case 1024:
+		return ICNS_1024x1024_32BIT_ARGB_DATA;
 		break;
 		
 	}

commit 921f972c461c505e5ac981aaddbdfdde97e8bb2b

Add missing comma to the list of image size strings
Also add whitespace to the next line for alignment.

Fixes: commit 78e85226cc9c0c38394c08eba9a0dc3fb8dc3c50
Reported-by: dcb (dcb314 on SourceForge)
Reported-in: https://sourceforge.net/p/icns/bugs/21/
Detected-by: clang 14
Warning:
  suspicious concatenation of string literals in an array initialization;
  did you mean to separate the elements with a comma? [-Wstring-concatenation]

icnsutils/icns2png.c
@@ -68,8 +68,8 @@ int	extractIconDepth = ALL_DEPTHS;
 /* Optional output directory */
 char    *outputPath = NULL;
 
-const char *sizeStrs[] =  { "1024", "1024x1024" "512", "512x512", "256", "256x256", "128", "128x128", "48", "48x48", "32", "32x32", "16", "16x16", "16x12"    };
-const int   sizeVals[] =  {  1024,   1024,       512,   512,       256,   256,       128,   128,       48,   48,      32,   32,      16,   16,      MINI_SIZE };
+const char *sizeStrs[] =  { "1024", "1024x1024", "512", "512x512", "256", "256x256", "128", "128x128", "48", "48x48", "32", "32x32", "16", "16x16", "16x12"    };
+const int   sizeVals[] =  {  1024,   1024,        512,   512,       256,   256,       128,   128,       48,   48,      32,   32,      16,   16,      MINI_SIZE };
 
 const char *depthStrs[] = { "32", "8", "4", "1" };
 const int   depthVals[] = {  32 ,  8 ,  4 ,  1  };